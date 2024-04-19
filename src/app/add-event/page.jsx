345e8f698b29173b94d99e671c5eb61e01fba264
Jonas Holbech <jh@jonasholbech.dk>
2024-04-19 10:01:02 +0200

    post from add event

---
+++ src/app/add-event/page.jsx
@@ -0,0 +1,49 @@
+import { redirect } from "next/navigation";
+export default async function AddEventPage() {
+  async function submit(formData) {
+    "use server";
+    let headersList = {
+      Accept: "application/json",
+      apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      Prefer: "return=representation",
+      "Content-Type": "application/json",
+    };
+
+    let bodyContent = JSON.stringify({
+      name: formData.get("name"),
+      when: formData.get("when"),
+      description: formData.get("description"),
+    });
+
+    let response = await fetch(
+      "https://uwrwptibotlxlvcdeicv.supabase.co/rest/v1/events",
+      {
+        method: "POST",
+        body: bodyContent,
+        headers: headersList,
+      }
+    );
+
+    let data = await response.json();
+    //console.log(data);
+    const id = data[0].id;
+    redirect("/events/" + id);
+  }
+  return (
+    <form action={submit}>
+      <div className="formcontrol">
+        <label htmlFor="form_name">Titel</label>
+        <input id="form_namne" type="text" name="name" />
+      </div>
+      <div className="formcontrol">
+        <label htmlFor="form_when">Hvornår</label>
+        <input id="form_when" type="date" name="when" />
+      </div>
+      <div className="formcontrol">
+        <label htmlFor="form_description">Andet du vil sige?</label>
+        <input id="form_description" type="text" name="description" />
+      </div>
+      <button>Gem</button>
+    </form>
+  );
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="flex gap-4">
+          <Link href="/">Hjem</Link>
+          <Link href="/add-event">Tilføj event</Link>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
