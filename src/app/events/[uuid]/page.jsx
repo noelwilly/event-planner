3871c37cd0a4fe40491b622a13529541246002dd
NoelWilliam <noelwilliamm@gmail.com>
2024-04-19 11:12:13 +0200

    noel eventplanner

--- src/app/events/[uuid]/page.jsx
+++ src/app/events/[uuid]/page.jsx
@@ -1,6 +1,5 @@
 export default async function EventPage({ params }) {
   const uuid = params.uuid;
-<<<<<<< HEAD
   console.log(uuid);
   let headerList = {
     Accept: "application/json",
@@ -13,21 +12,6 @@ export default async function EventPage({ params }) {
   let data = await response.json();
   console.log(data);
   const eventInfo = data[0];
-
-=======
-  let headersList = {
-    Accept: "application/json",
-    apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  };
-  let response = await fetch(
-    "https://uwrwptibotlxlvcdeicv.supabase.co/rest/v1/events?id=eq." + uuid,
-    {
-      headers: headersList,
-    }
-  );
-  let data = await response.json();
-  const eventInfo = data[0];
->>>>>>> f61aaed47ee4cf1a342d1b0b67ef347113e84267
   return (
     <article>
       <h1>{eventInfo.name}</h1>
